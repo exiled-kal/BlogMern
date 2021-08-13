983c7dd121274db7879e00414d3a623bc06bc8ae
kalden <tseringkalden1964@gmail.com>
2021-08-12 20:16:50 -0700

    initiated with google firebase

---
+++ client/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD5QqzlpKa32O2aq6LsP8PZnzsoE9nQRlI",
+  authDomain: "blog-79198.firebaseapp.com",
+  projectId: "blog-79198",
+  storageBucket: "blog-79198.appspot.com",
+  messagingSenderId: "513235612737",
+  appId: "1:513235612737:web:1ed88c866357ae727282c0",
+  measurementId: "G-N5Q69BPEJ0",
+};
